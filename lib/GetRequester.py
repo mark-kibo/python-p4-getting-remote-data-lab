c0ab67989a67c9a29e9d93a551552d7fdf3c40e6
mark-kibo <kibochamark@gmail.com>
2023-10-21 03:51:42 -0700

    Completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            return response.content
+        else:
+            return "invalid"
 
     def load_json(self):
-        pass
+        my_data=self.get_response_body()
+        return json.loads(my_data)
